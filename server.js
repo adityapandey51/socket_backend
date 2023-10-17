e3c3f361eb33ddb570d60be970e4e3d46b06fa2a
Aditya Pandey <adityapandey3082004@gmail.com>
2023-10-17 22:37:12 +0530

    path import

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import chatRoutes from "./Routes/chatRoutes.js"
 import messageRoutes from "./Routes/messageRoutes.js"
 import { errorHandler, notFound } from "./middleware/errormiddleware.js";
 import SocketIo from "socket.io";
+import path from "path";
 
 dotenv.config();
 
@@ -25,6 +26,26 @@ app.get("/",(req,res)=>{
 app.use("/api/v1/users",userRoute)
 app.use("/api/v1/chats",chatRoutes)
 app.use("/api/v1/message",messageRoutes)
+
+// --------------DEPLOYMENT---------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// AND ALONG WITH THIS YOU NEED TO ADD THIS TO PACKAGE.JSON
+    // "build": "npm install --legacy-peer-deps && npm install --legacy-peer-deps --prefix frontend && npm run build --prefix frontend"
+
+// -----------------------------------------------------------
 
 app.use(notFound)
 app.use(errorHandler)
